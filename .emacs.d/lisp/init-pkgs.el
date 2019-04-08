ce182a08e860769ef3248d16e082a8160aa8ecca
robertzhouxh <robertzhouxh@gmail.com>
2019-04-08 21:47:50 +0800

    adjust eshell config

--- .emacs.d/lisp/init-pkgs.el
+++ .emacs.d/lisp/init-pkgs.el
@@ -267,72 +267,50 @@
 
 (use-package logview :ensure t)
 (use-package hydra :ensure t)
-;; reffer to http://jwintz.me/blog/2014/02/16/helm-dash-makes-you-efficient/
-;(use-package helm-dash
-;             :ensure t
-;             :defines (helm-dash-docsets)
-;             :functions (esk-helm-dash-install
-;                          helm-dash-web
-;                          helm-dash-go
-;                          helm-dash-installed-docsets)
-;             :commands (helm-dash-at-point esk-helm-dash-install)
-;             :preface
-;             (progn
-;               (defvar esk-dash-docsets
-;                 ;'("Bash" "C" "C++" "Go" "Redis" "Ansible" "UnderscoreJS" "JavaScript" "React"))
-;                 '("Bash" "Go" "Clojure"))
-;
-;               (defun esk-helm-dash-install (docset-name)
-;                 (message (format "Installing helm-dash docset '%s'" docset-name))
-;                 (unless (file-exists-p (concat (concat helm-dash-docsets-path docset-name) ".docset"))
-;                   (helm-dash-install-docset docset-name)))
-;
-;               (defun esk-dash-limit (docsets-names)
-;                 (set (make-local-variable 'helm-dash-docsets) docsets-names))
-;
-;               (defun helm-dash-bash () (esk-dash-limit '("Bash")))
-;               (defun helm-dash-go () (esk-dash-limit '("Go" "Redis")))
-;               (defun helm-dash-clojure () (esk-dash-limit '("Clojure")))
-;               (defun helm-dash-yaml () (esk-dash-limit '("Ansible")))
-;               (defun helm-dash-c () (esk-dash-limit '("c")))
-;               (defun helm-dash-web () (esk-dash-limit '("UnderscoreJS" "JavaScript" "React")))
-;
-;               :init
-;               (progn
-;                 (setq helm-dash-docsets-path "~/.emacs.d/docsets/")
-;                 (after sh-script (add-hook 'sh-mode-hook 'helm-dash-bash))
-;                 (after go-mode (add-hook 'go-mode-hook 'helm-dash-go))
-;                 (after clojure-mode (add-hook 'clojure-mode-hook 'helm-dash-clojure))
-;                 ;(after yaml-mode (add-hook 'yaml-mode-hook 'helm-dash-yaml))
-;                 ;(after c-mode (add-hook 'c-mode-hook 'helm-dash-c))
-;                 ;(after web-mode (add-hook 'web-mode-hook 'helm-dash-web))
-;                 )
-;               :config
-;               (progn
-;                 (defun eww-split (url)
-;                   (interactive)
-;                   (select-window (split-window-right))
-;                   (eww url))
-;                 (setq helm-dash-browser-func 'eww-split)
-;                 ;(setq helm-dash-browser-func 'eww)
-;                 (add-hook 'prog-mode-hook
-;                           (lambda ()
-;                             (interactive)
-;                             (setq helm-current-buffer (current-buffer))))
-;                 (dolist (docset esk-dash-docsets)
-;                   (esk-helm-dash-install docset))
-;                 )))
+;;(use-package eshell
+;;             :ensure t
+;;             :config
+;;             (require 'f)
+;;             (setq eshell-visual-commands
+;;                   '("less" "tmux" "htop" "top" "bash" "zsh" "fish"))
+;;             (setq eshell-visual-subcommands
+;;                   '(("git" "log" "l" "diff" "show")))
+;;             ;; Prompt with a bit of help from http://www.emacswiki.org/emacs/EshellPrompt
+;;             (defmacro with-face (str &rest properties)
+;;               `(propertize ,str 'face (list ,@properties)))
+;;             (defun eshell/abbr-pwd ()
+;;               (let ((home (getenv "HOME"))
+;;                     (path (eshell/pwd)))
+;;                 (cond
+;;                   ((string-equal home path) "~")
+;;                   ((f-ancestor-of? home path) (concat "~/" (f-relative path home)))
+;;                   (path))))
+;;             (defun eshell/my-prompt ()
+;;               (let ((header-bg "#161616"))
+;;                 (concat
+;;                   (with-face (eshell/abbr-pwd) :foreground "#008700")
+;;                   (if (= (user-uid) 0)
+;;                     (with-face "#" :foreground "red")
+;;                     (with-face "$" :foreground "#2345ba"))
+;;                   " ")))
+;;             (setq eshell-prompt-function 'eshell/my-prompt)
+;;             (setq eshell-highlight-prompt nil)
+;;             (setq eshell-prompt-regexp "^[^#$\n]+[#$] ")
+;;             (setq eshell-cmpl-cycle-completions nil))
 (use-package eshell
              :ensure t
              :config
              (require 'f)
              (setq eshell-visual-commands
                    '("less" "tmux" "htop" "top" "bash" "zsh" "fish"))
+
              (setq eshell-visual-subcommands
                    '(("git" "log" "l" "diff" "show")))
+
              ;; Prompt with a bit of help from http://www.emacswiki.org/emacs/EshellPrompt
              (defmacro with-face (str &rest properties)
                `(propertize ,str 'face (list ,@properties)))
+
              (defun eshell/abbr-pwd ()
                (let ((home (getenv "HOME"))
                      (path (eshell/pwd)))
@@ -340,9 +318,12 @@
                    ((string-equal home path) "~")
                    ((f-ancestor-of? home path) (concat "~/" (f-relative path home)))
                    (path))))
+
              (defun eshell/my-prompt ()
                (let ((header-bg "#161616"))
                  (concat
+                   (with-face user-login-name :foreground "#d75faf")
+                   " "
                    (with-face (eshell/abbr-pwd) :foreground "#008700")
                    (if (= (user-uid) 0)
                      (with-face "#" :foreground "red")
