d3f3077930fa8253ba03adec4b882962ff645d13
robertzhouxh <robertzhouxh@gmail.com>
2019-04-23 10:44:37 +0800

    adjust brew.sh exec chain

--- brew.sh
+++ brew.sh
@@ -102,8 +102,9 @@ brew install bash-completion2
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash
 fi;
+
+chsh -s /usr/local/bin/bash
 
 brew install wget
 brew install openssl
