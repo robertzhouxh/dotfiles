458b3b567ab280b50e8ed29a38257423b9254204
robertzhouxh <robertzhouxh@gmail.com>
2020-10-26 22:00:40 +0800

    fix bug for package cl is depricated

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,4 @@
 ;; set garbage collection threshold high for initialization
 (setq gc-cons-threshold (* 100 1000 1000))
+(setq byte-compile-warnings '(not cl-functions obsolete))
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
