769aa7ce7dec96e492361187d62fb11b6115521d
robertzhouxh <robertzhouxh@gmail.com>
2020-12-25 19:12:02 +0800

    adjust emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo -v
-echo -e "\033[40;32m install the emacs ... \033[0m"
-
 
 # gtags for emacs
 #  sudo -H pip install pygments
@@ -20,27 +17,4 @@ echo -e "\033[40;32m install the emacs ... \033[0m"
 
 # GCC-Emacs ---> https://github.com/jimeh/build-emacs-for-macos
 
-rm -rf ~/.emacs
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-lnif() {
-    if [ -e "$1" ]; then
-        ln -sf "$1" "$2"
-    fi
 
-    if [ -e "$1" ]; then
-	mkdir -p ~/.emacs.d
-        ln -sf "$1" "$2"
-    fi
-}
-
-echo -e "\033[40;32m Step1: Backing up current emacs config \033[0m"
-today=`date +%Y%m%d`
-# for i in $HOME/.emacs.d; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
-# for i in $HOME/.emacs.d; do [ -L $i ] && unlink $i ; done
-if [ -e "$HOME/.emacs.d" ]; then
-  unlink "$HOME/.emacs.d"
-fi
-echo -e "\033[40;32m Step2: Setting up symlinks \033[0m"
-lnif "$CURRENT_DIR/.emacs.d" "$HOME/.emacs.d"
-
-echo -e "\033[40;32m Done, Happy hacking With The Awesome Emacs \033[0m"
