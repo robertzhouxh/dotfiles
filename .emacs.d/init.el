f5583d3f7967c7e712ffc2f30fda7aca672efc81
周学浩(10033362) <zhouxh-e@glodon.com>
2021-04-16 18:43:55 +0800

    no vc-redifined warning

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,6 @@
 ;; set garbage collection threshold high for initialization
 (setq gc-cons-threshold (* 100 1000 1000))
 (setq byte-compile-warnings '(not cl-functions obsolete))
+(set 'ad-redefinition-action 'accept)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(put 'dired-find-alternate-file 'disabled nil)
