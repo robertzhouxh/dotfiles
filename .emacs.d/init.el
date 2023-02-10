e5bd094c3fb907281d849735e03f50d8bb9234a7
robertzhouxh <robertzhouxh@gmail.com>
2023-02-10 16:37:45 +0800

    add org-auto-tangle for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,6 @@
-(setq byte-compile-warnings '(cl-functions))
-(setq native-comp-async-report-warnings-errors nil)
+(require 'cl)          
+(require 'org)
+(require 'cl-lib)
+(setq gc-cons-threshold (* 200 1024 1024))
+(run-with-idle-timer 5 t #'garbage-collect)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
