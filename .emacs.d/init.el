4e2c7eccd87aaec8a864970977070cb201381d70
robertzhouxh <robertzhouxh@gmail.com>
2023-03-03 13:33:34 +0800

    rm yaml-mode and flycheck

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,3 +3,4 @@
 (setq gc-cons-threshold (* 200 1024 1024))
 (run-with-idle-timer 5 t #'garbage-collect)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(put 'dired-find-alternate-file 'disabled nil)
