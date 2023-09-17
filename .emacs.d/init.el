f18931ee5d69cb466ed72c566aeb4c4f99e8441a
zhouxh-e <zhouxh-e@glodon.com>
2023-09-17 17:58:31 +0800

    reconstruct

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,5 @@
 (require 'org)
 (require 'cl-lib)
-(setq gc-cons-threshold (* 200 1024 1024))
-(run-with-idle-timer 5 t #'garbage-collect)
+(setq byte-compile-warnings '(cl-functions))
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 (put 'dired-find-alternate-file 'disabled nil)
