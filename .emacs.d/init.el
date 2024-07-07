865d68fa887f770f922524b63db37539c62fa8c5
robertzhouxh <robertzhouxh@gmail.com>
2024-07-07 20:57:58 +0800

    update git config  for gitlab

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,3 +3,17 @@
 (setq byte-compile-warnings '(cl-functions))
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 (put 'dired-find-alternate-file 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("67f6b0de6f60890db4c799b50c0670545c4234f179f03e757db5d95e99bac332" "9f2a181c19c10162f6135a007443d5e7e5524070f7aedd5d4c442cc80e7d3ebe" default))
+ '(tramp-verbose 0))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
