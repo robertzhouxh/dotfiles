712f28f1126f91ddba543b4deebb936d0f483ca1
zxh <robertzhouxh@gmail.com>
2026-05-04 20:32:37 +0800

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,15 +8,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("0f4e8712faa97372f505cac514d11f7fa0891e1dfc8bdf03208247b31fa29a01"
-     default))
- '(highlight-parentheses-colors '("DarkOrange" "DeepSkyBlue" "DarkRed") nil nil "Customized with use-package highlight-parentheses"))
+ '(package-vc-selected-packages
+   '((claude-code :url "https://github.com/stevemolitor/claude-code.el"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(vundo-highlight ((t (:foreground "#FFFF00"))))
- '(vundo-node ((t (:foreground "#808080"))))
- '(vundo-stem ((t (:foreground "#808080")))))
+ )
